43560710827afdc8ba67fb372a9227581a280b88
ian <ianthekid@gmail.com>
2020-06-24 15:05:51 -0700

    theme support funtions

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
 <?php
 //
+add_theme_support('post-thumbnails' ;
+add_theme_support('align-wide');
+add_theme_support('responsive-embeds');
 
+if ( function_exists( 'register_nav_menus' ) ) {
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' ),
+      'footer-menu' => __( 'Footer Menu' )
+    )
+  );
+}
 ?>
